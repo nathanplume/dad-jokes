b8e5d78948c8c50f007e23080c5c6f66b1249aae
Nathan Plume <nathan.plume@gmail.com>
2021-12-24 07:52:58 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,35 @@
+const jokeEl = document.querySelector('#joke');
+const jokeBtn = document.querySelector('#jokeBtn');
+
+jokeBtn.addEventListener('click', generateJoke);
+
+generateJoke()
+
+//using async await
+async function generateJoke() {
+    const config = {
+        headers: {
+            Accept: 'application/json'
+        }
+    }
+    const res = await fetch('https://icanhazdadjoke.com', config)
+
+    const data = await res.json();
+
+    jokeEl.innerHTML = data.joke
+}
+
+// using .then
+// function generateJoke() {
+//     const config = {
+//         headers: {
+//             Accept: 'application/json'
+//         }
+//     }
+//     fetch('https://icanhazdadjoke.com', config)
+//         .then((res) => res.json())
+//         .then((data) => jokeEl.innerHTML = data.joke);
+// }
+
+
+
